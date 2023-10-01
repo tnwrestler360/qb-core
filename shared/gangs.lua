78e5eea162e5d21519a1ee7b58bb4ffb76b2914b
tnwrestler360 <tyler.davis4494@gmail.com>
2023-09-30 21:20:32 -0700

    initial commit

---
+++ shared/gangs.lua
@@ -0,0 +1,140 @@
+QBShared = QBShared or {}
+QBShared.Gangs = {
+	['none'] = {
+		label = 'No Gang',
+		grades = {
+            ['0'] = {
+                name = 'Unaffiliated'
+            },
+        },
+	},
+	['lostmc'] = {
+		label = 'The Lost MC',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
+	['ballas'] = {
+		label = 'Ballas',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
+	['vagos'] = {
+		label = 'Vagos',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
+	['cartel'] = {
+		label = 'Cartel',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
+	['families'] = {
+		label = 'Families',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
+	['triads'] = {
+		label = 'Triads',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
+    ['tuner'] = {
+		label = 'Tuners 6STR',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Mechanic'
+            },
+			['2'] = {
+                name = 'Painter'
+            },
+            ['3'] = {
+                name = 'Seller'
+            },
+			['4'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	}
+}
